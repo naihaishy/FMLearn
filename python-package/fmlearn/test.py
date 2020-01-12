e40ebf40655fb7f3edc68372f680bc646b2a20ba
naihai‘git config --global user.email 448435279@qq.comgit clone git@github.zhfsky.com:naihai/watermarker.gitcd watermarklstouch REAMDEMDgit config --global user.name naihai‘ <448435279@qq.com>
2020-01-12 21:40:19 +0800

    fix import

--- python-package/fmlearn/test.py
+++ python-package/fmlearn/test.py
@@ -2,7 +2,7 @@
 # @Time : 2020/1/12 17:25
 # @Author : naihai
 import pandas as pd
-from .data import DMatrix
+from fmlearn import DMatrix
 
 if __name__ == '__main__':
     data = pd.read_csv("E:\Scala\projects\Recommend\data\house_price_train.txt", sep='\t', header=None)
