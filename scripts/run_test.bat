84d9e5733c3d26fb8d533a3f86e08074c680a6d3
naihaishy <open@zhfsky.com>
2020-02-03 16:46:04 +0800

    [Cmake] update CMakeLists.txt

--- scripts/run_test.bat
+++ scripts/run_test.bat
@@ -1,9 +1,12 @@
 @echo off
 REM # Test for running unit test
-.\src\c_api\c_api_test.exe
-.\src\common\common_test.exe
-.\src\data\data_test.exe
-.\src\core\model_test.exe
+.\test\c_api_test.exe
+.\test\common_test.exe
+.\test\cmdline_test.exe
+.\test\data_test.exe
+.\test\model_test.exe
+.\test\loss_test.exe
+.\test\score_test.exe
 fmlearn_train data\house_price_train.txt fm.model -lr 0.0001 -e 10
 fmlearn_predict data\house_price_test.txt fm.model
 @pause
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Script for running unit test
-./src/c_api/c_api_test
-./src/common/common_test
-./src/data/data_test
-./src/core/model_test
-./src/cli/cli_test
-./src/loss/loss_test
-./src/score/score_test
+./test/c_api_test
+./test/common_test
+./test/cmdline_test
+./test/data_test
+./test/model_test
+./test/loss_test
+./test/score_test
 ./fmlearn_train data/house_price_train.txt fm.model -lr 0.0001 -e 10
 ./fmlearn_predict data/house_price_test.txt fm.model
